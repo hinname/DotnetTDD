118b773562438f4d6a55a9e6ee5589ac88efe880
hinname <herivelton.pro@gmail.com>
2023-11-25 12:37:02 -0300

    Add project

---
+++ SimpleApp.Api/Controllers/FansController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+namespace SimpleApp.Api.Controllers
+    {
+    [ApiController]
+    [Route("[controller]")]
+    public class FansController : ControllerBase
+        {
+        [HttpGet(Name = "GetFans")]
+        public async Task<IActionResult> Get()
+            {
+            return Ok("fans");
+            }
+        }
+    }
